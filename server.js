2fc5cbb239427d5028dcd10db0a3626dd00d6e27
Yashwanth Thota <31579270+YashwanthThota@users.noreply.github.com>
2018-08-06 21:13:19 -0500

    heroku modified

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/home-first-app'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/home-first-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log('Console Listening!');
